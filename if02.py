a7fb9a5046f49723200e641ad61bdf11b6039386
bahriddinovfirdavs <bahridinovfirdavs017@gmail.com>
2023-09-02 15:09:24 +1030

    if02.py

--- if02.py
+++ if02.py
@@ -8,4 +8,10 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    if a<b and a<c:
+        return "First number."
+    elif b<a and b<c:
+        return "Second number."
+    elif c<a and c<b:
+        return "Third number."
+print(main(8,4,2))
