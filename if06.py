7ab65969d327095b2e20a5786835b23ec9a1ef88
bahriddinovfirdavs <bahridinovfirdavs017@gmail.com>
2023-09-03 18:00:57 +1030

    if06.py

--- if06.py
+++ if06.py
@@ -6,4 +6,19 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    a=n//10000 
+    b=(n/1000)%10
+    c=(n//100)%10
+    d=(n//10)%10
+    e=n%10
+    if a>b and a>c and a>d and a>e:
+        return a
+    elif b>a and b>c and b>d and b>e:
+        return b
+    elif c>b and c>a and c>d and c>e:
+        return c
+    elif d>b and d>c and d>a and d>e:
+        return d
+    else:
+        return e
+print(" Five-digit number.",main(int(input())))
